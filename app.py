e842c24743e931fa26094d97b83f3bf15126d2c2
Shreeya Khadka <khadkashreya.sk@gmail.com>
2024-07-01 17:11:08 +0545

    Edited some files

--- app.py
+++ app.py
@@ -25,11 +25,11 @@ APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 @app.route('/')
 def hello_world():
-    return render_template('index.html')
+   return render_template('index.html')
 
 
 @app.route('/photo', methods=['GET', 'POST'])
-def upload():
+def upload_file():
     path = "static/outimg"
     imgToRemove = os.listdir(path)
     print(imgToRemove)
@@ -150,6 +150,160 @@ def preprocessing(img):
 
     return each_word_segmentation
 
+def dikka_remove(output): #Needed for Word segmentation
+    resultafterdikka = []
+    each_character = []
 
-if __name__ == "__main__":
+    for i in range(0, len(output)):
+        each = []
+        main = output[i]
+        r, inv3 = cv.threshold(main, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)
+
+        ig = output[i]
+        row, col = ig.shape
+
+ #Detects and removes the largest horizontal line (referred to as "DIKA") in an image
+        horizontal_kernel = cv.getStructuringElement(cv.MORPH_RECT, (40, 1))
+        detect_horizontal = cv.morphologyEx(ig, cv.MORPH_OPEN, horizontal_kernel, iterations=2)
+        cnts, _ = cv.findContours(detect_horizontal, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+        if len(cnts) > 0:
+            c = max(cnts, key=cv.contourArea)
+            X, Y, w, h = cv.boundingRect(c)
+            ig[0:Y + h + 2, 0:X + w].fill(0)
+
+            r, inv1 = cv.threshold(ig, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)
+            cnts1, _ = cv.findContours(inv1, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+
+            for co in reversed(cnts1):
+                if cv.contourArea(co) > 300:
+                    X, Y, w, h = cv.boundingRect(co)
+                    cv.rectangle(inv3, (X, 0), (X + w, Y + h), 255, 1)
+                    each.append((inv3[0:Y + h, X:X + w]))
+            each_character.append(each)
+            resultafterdikka.append(inv3)
+
+    return resultafterdikka, each_character
+
+
+@app.route('/cleanup')
+def cleanup_file():
+    path = "static/outimg"
+    willremoveimage = os.listdir(path)
+    if not willremoveimage:
+        pass
+    else:
+        for i in willremoveimage:
+            os.remove(path + "/" + i)  #For each file,deletes the file from the directory.
+
+    return render_template('index.html', title='Devnagarik - Home')
+
+#MERGED
+#@app.route('/')
+# def index():
+#     # Cleanup the directory
+#     path = "static/outimg"
+#     willremoveimage = os.listdir(path#
+#     if not willremoveimage:
+#         pass
+#     else:
+#         for i in willremoveimage:
+#             os.remove(path + "/" + i)  # For each file, deletes the file from the directory.
+
+#     return render_template('index.html', title='Devnagarik - Home')
+# 
+
+
+@app.route('/send_pic', methods=['POST'])
+def button_pressed(): #letter detect and predict
+    # print("Image recieved")
+    dimensions = (100, 100)
+    data_url = request.values['imgBase64']
+    encoded_data = data_url.split(',')[1]
+    nparr = np.fromstring(base64.b64decode(encoded_data), np.uint8)
+    img = cv.imdecode(nparr, cv.IMREAD_COLOR)
+
+    name = list(string.ascii_letters)
+    word = preprocessing(img)
+    char = ""
+    for i in range(len(word)):
+        cv.imwrite("static/outimg/image-" + name[i] + ".jpg", word[i])
+
+    total_count = len(word)
+    print("*" * 10)
+    print("Total letters found: ", total_count)
+    for count, i in enumerate(range(total_count), start=1):
+        print("Performing operation for: ", count)
+        resize = cv.resize(word[i], (32, 32)) / 255.0
+        reshaped = np.reshape(resize, (1, 32, 32, 1))
+
+        prediction = newmodel.predict(reshaped)
+        score_prediction = prediction > 0.5
+        probab = str(np.amax(prediction))
+        max = score_prediction.argmax()
+        predict_character = devnagarik_word[max]
+        char += predict_character
+        print("Predicted character", predict_character)
+        print("Predicted character index", max)
+        print("Probab", probab)
+        print("*" * 10)
+    print("End !!")
+    print("*" * 10)
+    return char
+
+@app.route('/uploader', methods=['GET', 'POST'])
+def upload(): #Word detect and predict
+    path = "static/outimg"
+    willremoveimage = os.listdir(path)
+    print(willremoveimage)
+    for i in willremoveimage:
+        os.remove(path + "/" + i)
+
+    target = os.path.join(APP_ROOT, 'static/images/')
+    # print(target)
+    # l=[]
+    if not os.path.isdir(target):
+        os.mkdir(target)
+
+    if request.method == 'POST':
+        # for file in request.files.getlist("file"):// for multiple file uplaod
+        file = request.files['file']
+        filename = file.filename
+        destination = "/".join([target, filename])
+        # print(destination)
+        file.save(destination)
+        newDes = os.path.join('static/images/' + filename)
+        readingimg = cv.imread(newDes)
+
+        def prediction(each_character):
+            final_all_word = ""
+            for i in range(len(each_character)):
+                each_word = ""
+                for j in each_character[i]:
+                    character_img = j
+                    resize = cv.resize(j, (32, 32)) / 255.0;
+                    reshaped = np.reshape(resize, (1, 32, 32, 1))
+
+                    prediction = newmodel.predict(reshaped)
+                    max = prediction.argmax()
+                    predict_character = devnagarik_word[max]
+                    each_word += predict_character
+                final_all_word += each_word + ' '
+            return final_all_word
+
+        output = preprocessing(readingimg)
+        resultafterdikka, each_character = dikka_remove(output)
+        final_result = prediction(each_character)
+
+        makingimagename = list(string.ascii_letters)
+
+        for i in range(len(resultafterdikka)):
+            cv.imwrite("static/outimg/image-" + makingimagename[i] + ".jpg", resultafterdikka[i])
+
+        images = os.listdir("static/outimg")
+
+    return render_template('index.html', photos=newDes, images_name=images, result=final_result,
+                           title='Devnagrik - Predict')
+
+
+if __name__ == '__main__':
     app.run(debug=True)
